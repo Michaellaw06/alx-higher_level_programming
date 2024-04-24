0ab20eb397cb522fe8d1d094e90b5d747d9defd3
tronzy <michaellawal06@gmail.com>
2024-04-24 19:42:39 +0100

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str_value = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str_result = str_value[39:67] + str_value[112:119] + str_value[:6]
+print(str_result)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+This  is a readme file for Python Project
+=======
+## python more data structure
+>>>>>>> a2511fd (Initial commit)
